a72cecffe6ac6b4b3fa6ce9fda2b9f50ba2ab388
khatunakhomasuridze08 <99075136+khatunakhomasuridze08@users.noreply.github.com>
2022-04-04 15:23:07 +0400

    add variables, simple

---
+++ app.js
@@ -0,0 +1,40 @@
+var userName = "khato";
+let lastName = "khomasuridze";
+let age = 26;
+let monthOfBirth = 10;
+const dayOfBirth = 11;
+const nameOfMonth = "october";
+
+// console.log(userName,lastName,age)
+
+lastName = "kvijhashvili";
+
+// console.log(userName,lastName)
+
+let myName ="khatuna";
+let myHobby = "paint";
+let yearOfBirth = 1995;
+let currentYear = 2022;
+let myNum = 11;
+let isFemale = true;
+let isArtist = false;
+let carName 
+
+// console.log("carName", typeof carName)
+// console.log("isArtist", typeof isArtist)
+// console.log("currentYear", typeof currentYear)
+
+console.log("My hobby is " + myHobby);
+console.log("My name is " + myName);
+console.log("I am " + (currentYear - yearOfBirth) + " year(s) old");
+
+
+let ageData = `I am ${currentYear - yearOfBirth} year(s) old`;
+console.log(ageData);
+
+let mySimpleText = `my hobby is ${myHobby}`;
+console.log(mySimpleText);
+
+let hobby = `my hobby is ${myHobby}`;
+console.log(hobby);
+
